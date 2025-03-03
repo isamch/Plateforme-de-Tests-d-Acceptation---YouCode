71fedfa7fbaad83bb20897a0de57c68523fb64ad
isam chajia <isamchajiabeat.com>
2025-03-03 16:36:02 +0000

    fix errors

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests\LoginRequest;
+use Illuminate\Support\Facades\Auth;
+
+
+
+
+class LoginController extends Controller
+{
+
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    public function login(LoginRequest $request)
+    {
+
+        $credentials = [
+            'email' => $request->input('email'),
+            'password' => $request->input('password'),
+        ];
+
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            $user = Auth::user();
+
+            if ($user->hasRole('admin')) {
+                return redirect()->intended('/home')->with('success', 'Welcome Admin!');
+            }
+
+            if ($user->hasRole('staff')) {
+                return redirect()->intended('/staff/dashboard')->with('success', 'Welcome Staff!');
+            }
+
+            if ($user->hasRole('candidat')) {
+                return redirect()->intended('/home')->with('success', 'Welcome Candidat!');
+            }
+
+        }
+
+
+        return redirect()->to('/login')->withErrors(['login_error' => 'Login failed!']);
+    }
+
+
+
+    public function logout(Request $request) {
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect()->to('/login')->with('success', 'Logout success!');
+    }
+
+}
